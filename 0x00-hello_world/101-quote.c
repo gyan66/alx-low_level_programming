8592c8e2829db9b17a9d7c805303380def310f9a
Alex Gyan <gyanalex447@gmail.com>
2022-10-14 21:51:49 -0700

    created 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,41 @@
-#include <stdio.h>
+/*
+ *
+ *  * File: 101-quote.c
+ *
+ *   * Auth: Brennan D Baraban
+ *
+ *    */
 
 
+
+#include <unistd.h>
+
+
+
+/**
+ *
+ *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *
+ *   *                followed by a new line, to standard error.
+ *
+ *    *
+ *
+ *     * Return: Always 1.
+ *
+ *      */
 
 int main(void)
+
 {
-       	fwrite(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 61);
+
+		write(2,
+
+					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+
+					      	      59);
 
 
 
-	return (1);
+			return (1);
+
 }
