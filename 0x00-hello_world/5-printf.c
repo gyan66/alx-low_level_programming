55925af1e280d287fc62490e2b7e6be2e10cf040
Alex Gyan <gyanalex447@gmail.com>
2022-10-14 20:33:45 -0700

    created 5-printf.c code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
-{ #include <stdio.h>
-	int main(void) {
-		printf("with proper grammer,but the out come is a piece of art,\n") ;
-		return(0) ;
-	}}
+#include <stdio.h>
+int main(void) {
+	printf("with proper grammer,but the outcome is a piece of art,\n") ;
+	return(0) ;
+}
