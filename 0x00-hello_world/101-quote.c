a60d86ad94e13778f927be7a27b9c05bd2667f98
Alex Gyan <gyanalex447@gmail.com>
2022-10-14 21:37:20 -0700

    created 101-quote.c code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,10 @@
-#include <stdio.h>
-int main(void)
-{
-	write(2, "and thta piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+ #include <stdio.h>
+
+
+
+int main(void) { write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 61);
+
+
+
 	return (1);
 }
